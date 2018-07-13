44a62196bae845afc310d4acda51b09f46bcbca4
Jordan <jordan@daydream-lab.com>
2018-07-13 18:53:39 +0800

    fixed bugs

--- src/Controllers/BaseController.php
+++ src/Controllers/BaseController.php
@@ -2,9 +2,15 @@
 
 namespace DaydreamLab\JJAJ\Controllers;
 
+use DaydreamLab\JJAJ\Services\BaseService;
 use Illuminate\Routing\Controller;
 
 class BaseController extends Controller
 {
+    protected $service;
 
+    public function __construct(BaseService $service)
+    {
+        $this->service = $service;
+    }
 }
