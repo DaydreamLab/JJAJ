204a201ea56b291bae5521522aef242f3df58877
Jordan <jordan@daydream-lab.com>
2018-07-20 18:50:02 +0800

    fixed bug

--- src/Services/BaseService.php
+++ src/Services/BaseService.php
@@ -50,7 +50,7 @@ class BaseService
     public function remove(Collection $input)
     {
         foreach ($input->ids as $id) {
-            $result = $this->repo->delete($id['id']);
+            $result = $this->repo->delete($id);
             if (!$result) {
               break;
             }
