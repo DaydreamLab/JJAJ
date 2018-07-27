6dbe24df0b907f31afc70efcda3dc649f980309d
Jordan <jordan@daydream-lab.com>
2018-07-27 18:23:37 +0800

    fixed bug

--- src/Repositories/BaseRepository.php
+++ src/Repositories/BaseRepository.php
@@ -51,7 +51,7 @@ class BaseRepository implements BaseRepositoryInterface
     {
         $model = $this->model;
         foreach ($fields as $key => $field) {
-            $model = $model->where($field[$key] , $operators[$key], $values[$key]);
+            $model = $model->where($field , $operators[$key], $values[$key]);
         }
         return $model->get();
     }
