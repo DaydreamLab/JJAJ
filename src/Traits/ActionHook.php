f556cbd56684289127094b135f0aa6a7e4cabc97
Jordan <jordan@daydream-lab.com>
2022-09-30 14:29:54 +0800

    fix: ActionHook input 改成用 reference

--- src/Traits/ActionHook.php
+++ src/Traits/ActionHook.php
@@ -8,53 +8,44 @@ trait ActionHook
 {
     public function afterCheckItem(Collection $input, $item)
     {
-
     }
 
     public function afterAdd(Collection $input, $item)
     {
-
     }
 
 
     public function afterModify(Collection $input, $item)
     {
-
     }
 
 
     public function afterRemove(Collection $input, $item)
     {
-
     }
 
 
     public function afterState(Collection $input, $item)
     {
-
     }
 
 
     public function beforeAdd(Collection &$input)
     {
-
     }
 
 
-    public function beforeModify(Collection $input, &$item)
+    public function beforeModify(Collection &$input, &$item)
     {
-
     }
 
 
-    public function beforeRemove(Collection $input, $item)
+    public function beforeRemove(Collection &$input, $item)
     {
-
     }
 
 
     public function beforeState($state, $item)
     {
-
     }
 }
